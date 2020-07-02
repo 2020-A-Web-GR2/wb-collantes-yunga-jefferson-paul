80ddeb202d02fc5f4cb037759288a064eb50c61d
Paul Collantes <jeffersoncollantes31@gmail.com>
2020-07-02 18:14:56 -0500

    creacion de archivos

--- 01-http/introduccion/src/main.ts
+++ 01-http/introduccion/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  /*Cualquier configuracion se realiza aqui antes de que arranque el servidosn*/
+
+  await app.listen(3001);
 }
 bootstrap();
