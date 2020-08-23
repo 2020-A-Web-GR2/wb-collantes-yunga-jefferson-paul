e08d9b0bfbb93962c0440d2d997d81f9a3ad5f5f
Paul Collantes <jeffersoncollantes31@gmail.com>
2020-08-23 18:59:34 -0500

    crear archivos publicos

--- 01-http/introduccion/src/main.ts
+++ 01-http/introduccion/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule) as any;
   /*Cualquier configuracion se realiza aqui antes de que arranque el servidosn*/
   app.use(cookieParser('Me agradan los poliperros'));
-  app.set('view engine','ejs');
+  app.set('view engine','ejs');// sera el motor a utilizar
   app.use(express.static('publico'));
   await app.listen(3001);
 }
